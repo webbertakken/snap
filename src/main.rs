694284f8c375ffcc294b446c33eaca7b8b35fb71
Webber <webber@takken.io>
2023-02-21 01:07:56 +0100

    chore: configure font

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use eframe::{run_native, App, NativeOptions};
-use egui::{CentralPanel, SidePanel, TopBottomPanel};
+use eframe::{run_native, App, CreationContext, Frame, NativeOptions};
+use egui::{
+    CentralPanel, Context, FontData, FontDefinitions, FontFamily, SidePanel, TopBottomPanel,
+};
 
 mod center_widget;
 mod footer;
@@ -15,7 +17,7 @@ fn main() -> Result<(), eframe::Error> {
     run_native(
         "Snap",
         native_options,
-        Box::new(|_cc| Box::new(Snap::default())),
+        Box::new(|cc| Box::new(Snap::new().init(cc))),
     )
 }
 
@@ -24,17 +26,39 @@ struct Snap {
     footer: footer::Widget,
 }
 
-impl Default for Snap {
-    fn default() -> Self {
+impl Snap {
+    pub fn new() -> Self {
         Self {
             footer: footer::Widget::new(),
             header: header::Widget::new(),
         }
     }
+
+    pub fn init(self, cc: &CreationContext) -> Self {
+        self.configure_fonts(&cc.egui_ctx);
+
+        self
+    }
+
+    fn configure_fonts(&self, ctx: &Context) {
+        let mut font_def = FontDefinitions::default();
+        font_def.font_data.insert(
+            "MesloLGM".to_string(),
+            FontData::from_static(include_bytes!("../assets/MesloLGM.ttf")),
+        );
+
+        font_def
+            .families
+            .get_mut(&FontFamily::Monospace)
+            .unwrap()
+            .insert(0, "MesloLGM".to_string());
+
+        ctx.set_fonts(font_def);
+    }
 }
 
 impl App for Snap {
-    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         TopBottomPanel::top("top_panel")
             .exact_height(64.0)
             .show_separator_line(false)
