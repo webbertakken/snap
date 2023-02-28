187eb570927209387de9784e7b9b8c280a060eac
Webber <webber@takken.io>
2023-02-28 20:00:04 +0100

    feat: add menu bar

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,5 @@
+use egui::Layout;
+
 pub struct Widget {}
 
 impl Widget {
@@ -5,19 +7,27 @@ impl Widget {
         Self {}
     }
     pub fn update(&mut self, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
-            ui.label("button1");
-            ui.label("button2");
+        egui::menu::bar(ui, |ui| {
+            ui.with_layout(Layout::left_to_right(egui::Align::Center), |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("button1");
+                    ui.label("button2");
 
-            ui.separator();
+                    ui.separator();
 
-            ui.label("button3");
-            ui.label("button4");
+                    ui.label("button3");
+                    ui.label("button4");
 
-            ui.separator();
+                    ui.separator();
 
-            ui.label("button5");
-            ui.label("button6");
+                    ui.label("button5");
+                    ui.label("button6");
+                });
+            });
+
+            ui.with_layout(Layout::right_to_left(egui::Align::Center), |ui| {
+                ui.label("test");
+            });
         });
     }
 }
